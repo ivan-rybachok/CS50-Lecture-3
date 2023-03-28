c4fcc2876b166be2350f51a4471e7cc59c641d14
Ivan Rybachok <blackhusky2014@gmail.com>
2023-03-28 19:10:55 -0300

    first commit

---
+++ search.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    // int numbers[7];
+    // numbers[0] = 20;
+
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    string s = get_string("String: ");
+    for (int i = 0; i < 6; i++)
+    {
+        if (strcmp(strings[i], s) == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
